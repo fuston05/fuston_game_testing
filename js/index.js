b882e49d930fd7e9daac667d58f590b532275e28
fuston05 <fuston05@yahoo.com>
2019-11-22 17:24:38 -0500

    created a 'roll dice' function

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-// text based rpg game??
+function rollDice(  ){
+    // 2 normal 6 sided die
+    const max= 7;
+    const min= 1;
+    const die1= Math.floor(Math.random() * (max - min))+ min;
+    const die2= Math.floor(Math.random() * (max - min)) + min;
+    const dieTotal= die1 + die2;
+    return `Die1: ${die1}, Die2: ${die2} \n Total Roll: ${dieTotal}`;
+}//end func
+
+// console.log(rollDice());
