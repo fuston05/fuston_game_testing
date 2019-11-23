0d0957d3a70f8e57f10258e2895630908854ea39
fuston05 <fuston05@yahoo.com>
2019-11-22 22:39:00 -0500

    got linked js file to work properly, played around with DOM ele selections and rollDice()

--- js/index.js
+++ js/index.js
@@ -1,11 +1,23 @@
-function rollDice(  ){
+function rollDice(){
     // 2 normal 6 sided die
     const max= 7;
     const min= 1;
-    const die1= Math.floor(Math.random() * (max - min))+ min;
-    const die2= Math.floor(Math.random() * (max - min)) + min;
-    const dieTotal= die1 + die2;
-    return `Die1: ${die1}, Die2: ${die2} \n Total Roll: ${dieTotal}`;
+
+    //roll die 1
+    const die_1= Math.floor(Math.random() * (max - min))+ min;
+
+    //roll die 2
+    const die_2= Math.floor(Math.random() * (max - min)) + min;
+
+    //add the die together
+    const dieTotal= die_1 + die_2;
+    
+    return `-Die_1: ${die_1},\n-Die_2: ${die_2} \n\nTotal Roll: ${dieTotal}`;
+
+    //could make it to where we pass in the number of die to be rolled? Not sure if that could be a needed feature or not.
 }//end func
 
-// console.log(rollDice());
+// let poop= document.getElementById("outPut");
+// poop.textContent= rollDice();
+
+
